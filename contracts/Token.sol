4a985293d217cb85fe3a26e6df6aa278b67e6e3d
Timur Appaev <alloaolla@gmail.com>
2018-06-22 17:07:37 +0300

    emit, constructor and public keywords

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,11 +9,11 @@ contract Token is MintableToken {
     string public constant symbol = 'PRIX';
     uint8 public constant decimals = 8;
     
-    function transferFrom(address from, address to, uint256 value) returns (bool) {
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
         return super.transferFrom(from, to, value);
     }
 
-    function transfer(address to, uint256 value) returns (bool) {
+    function transfer(address to, uint256 value) public returns (bool) {
         return super.transfer(to, value);
     }
 
